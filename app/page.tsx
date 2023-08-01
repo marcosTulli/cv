238a127f4fad795c58ae62f6f41b0dd147caa4d2
Milton Tulli <milton.tulli@gmail.com>
2023-08-01 15:49:46 -0300

    suggestions

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,75 @@
-'use client';
-import * as React from 'react';
-import Image from 'next/image';
-import JobCard from './components/JobCard';
-import './styles/style.scss';
-import jobsData from './assets/jobs-data';
-import skillsData from './assets/skills-data';
-import educationData from './assets/education-data';
-import { IStrings } from './models/strings';
-import stringsEn from './loc/strings-en';
-import stringsSp from './loc/strings-sp';
+// TODO: Use eslint vscode extension
+"use client";
+import * as React from "react";
+import Image from "next/image";
+import JobCard from "./components/JobCard";
+import "./styles/style.scss";
+import jobsData from "./assets/jobs-data";
+import skillsData from "./assets/skills-data";
+import educationData from "./assets/education-data";
+import { IStrings } from "./models/strings";
+import stringsEn from "./loc/strings-en";
+import stringsSp from "./loc/strings-sp";
 
+// TODO: Put this somewere else.
 export enum Language {
-  EN = 'en',
-  SP = 'sp'
+  EN = "en",
+  SP = "sp",
 }
 
 const icons = {
   width: 20,
   height: 20,
 };
+
+// TODO: 2. This should be an internal fn of LanguageContext
 const getStringFile = (language: Language) => {
   switch (language) {
     case Language.EN:
       return stringsEn;
-    case Language.SP:
+    case Language.SP: // FIXME: Language.ES, not SP
       return stringsSp;
   }
 };
 
 export default function Home() {
-  const [currentLanguage, setCurrentLanguage] = React.useState<Language>(Language.EN);
-  const strings: IStrings = getStringFile(currentLanguage);
-  const [fileName, setFilename] = React.useState<string>(strings.CV_EN);
-  const fileURL = `http://localhost:3000/${fileName}.pdf`;
+  // TODO: 1. Convert this state into a context (LanguageContext) that extends: { lang: Language , setLang: (Language) => void, trans: {candidateName, ...}, downloadFile: () => Promise<void>  }.
+  const [currentLanguage, setCurrentLanguage] = React.useState<Language>(
+    Language.EN
+  );
+  const strings: IStrings = getStringFile(currentLanguage); // TODO: remove
+  const [fileName, setFilename] = React.useState<string>(strings.CV_EN); // TODO: remove
+  const fileURL = `http://localhost:3000/${fileName}.pdf`; // TODO: remove
 
   const toggleLanguage = () => {
     switch (currentLanguage) {
       case Language.EN:
         setCurrentLanguage(Language.SP);
-        setFilename(strings.CV_SP);
+        setFilename(strings.CV_SP); // This does not makes sense.
         break;
       default:
-        setFilename(strings.CV_EN);
+        setFilename(strings.CV_EN); // This does not makes sense.
         setCurrentLanguage(Language.EN);
         break;
     }
   };
 
   const handleDownloadClick = (url: string) => {
-    fetch(url).then(res => res.blob()).then(blob => {
-      const blobURL = window.URL.createObjectURL(new Blob([blob]));
-      const aTag = document.createElement('a');
-      aTag.href = blobURL;
-      aTag.setAttribute('download', `${fileName}.pdf`);
-      document.body.appendChild(aTag);
-      aTag.click();
-      aTag.remove();
-
-    });
+    // Move to download fn in context and refactor:
+    // You should not use string builder to build url with filename, just consume lang from context
+    fetch(url)
+      .then((res) => res.blob())
+      .then((blob) => {
+        const blobURL = window.URL.createObjectURL(new Blob([blob]));
+        const aTag = document.createElement("a");
+        aTag.href = blobURL;
+        aTag.setAttribute("download", `${fileName}.pdf`);
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
+      });
   };
-
+  // TODO: Split into smaller components
   return (
     <div className="cvContainer">
       <div className="leftSection">
@@ -70,61 +80,113 @@ export default function Home() {
         </div>
         <div className="workExperience">
           <div className="sectionTitle">{strings.workExperience}</div>
-          {jobsData.map(i => {
-            return (
-              <JobCard key={i.id} data={i} language={currentLanguage} />
-            );
+          {jobsData.map((i) => {
+            return <JobCard key={i.id} data={i} language={currentLanguage} />;
           })}
         </div>
       </div>
       <div className="rightSection">
         <div className="profile">
           <div className="profilePicture">
-            <Image src="/profile-picture.jpg" alt="profile picture" width={300} height={300} />
+            <Image
+              src="/profile-picture.jpg"
+              alt="profile picture"
+              width={300}
+              height={300}
+            />
           </div>
           <div className="contactInfo">
             <ul>
               <li>
-                <Image src='/location.png' alt='location-icon' width={icons.width} height={icons.height} />
+                <Image
+                  src="/location.png"
+                  alt="location-icon"
+                  width={icons.width}
+                  height={icons.height}
+                />
                 <p>{strings.location}</p>
                 <div>
-                  <p>EN</p>
+                  <p>EN</p> {/** TODO: This should be translated too */}
                   <label title="Switch Language" className="switch">
+                    {" "}
+                    {/** TODO: This title should be translated too */}
                     <input type="checkbox" onChange={toggleLanguage} />
                     <span className="slider round"></span>
                   </label>
                 </div>
-                <p>SP</p>
+                <p>SP</p> {/** TODO: This should be translated too */}
               </li>
               <li>
-                <Image src='/mail.png' alt='mail-icon' width={icons.width} height={icons.height} />
+                <Image
+                  src="/mail.png"
+                  alt="mail-icon"
+                  width={icons.width}
+                  height={icons.height}
+                />
                 <p>{strings.email}</p>
               </li>
 
               <li>
-                <Image src='/telephone.png' alt='phone-icon' width={icons.width} height={icons.height} />
+                <Image
+                  src="/telephone.png"
+                  alt="phone-icon"
+                  width={icons.width}
+                  height={icons.height}
+                />
                 <p>{strings.phone}</p>
               </li>
 
               <li>
                 <div>
-                  <Image src='/uk.png' alt='english' width={icons.width} height={icons.height} />
+                  <Image
+                    src="/uk.png"
+                    alt="english"
+                    width={icons.width}
+                    height={icons.height}
+                  />
                   <p>{strings.english}</p>
-                  <Image src='/spain.png' alt='spanish' width={icons.width} height={icons.height} />
+                  <Image
+                    src="/spain.png"
+                    alt="spanish"
+                    width={icons.width}
+                    height={icons.height}
+                  />
                   <p>{strings.spanish}</p>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href={strings.linkedinURL} target="_blank" title={strings.linkedinURL}>
-                    <Image src='/linkedin.png' alt='linkedin' width={icons.width} height={icons.height} />
+                  <a
+                    href={strings.linkedinURL}
+                    target="_blank"
+                    title={strings.linkedinURL}
+                  >
+                    <Image
+                      src="/linkedin.png"
+                      alt="linkedin"
+                      width={icons.width}
+                      height={icons.height}
+                    />
                     <p>{strings.linkedin}</p>
                   </a>
-                  <a href={strings.githubURL} target="_blank" title={strings.githubURL}>
+                  <a
+                    href={strings.githubURL}
+                    target="_blank"
+                    title={strings.githubURL}
+                  >
                     <p>{strings.github}</p>
                   </a>
-                  <button className='download' title="download" onClick={() => handleDownloadClick(fileURL)}>
-                    <Image src='/download.png' alt='download' width={40} height={40} />
+                  <button
+                    className="download"
+                    title="download"
+                    onClick={() => handleDownloadClick(fileURL)}
+                  >
+                    <Image
+                      src="/download.png"
+                      alt="download"
+                      width={40}
+                      height={40}
+                    />
                   </button>
                 </div>
               </li>
@@ -135,34 +197,29 @@ export default function Home() {
           <div className="sectionTitle">{strings.education}</div>
           <div className="educationCard">
             <ul>
-              {
-                educationData.map(i => {
-                  const education = currentLanguage === Language.SP ? i.sp : i.en;
-                  return (
-                    <li key={i.id}>
-                      <div className="degree">{education.title}</div>
-                      <p>{education.content}</p>
-                    </li>
-                  );
-                })
-
-              }
+              {educationData.map((i) => {
+                const education = currentLanguage === Language.SP ? i.sp : i.en;
+                return (
+                  <li key={i.id}>
+                    <div className="degree">{education.title}</div>
+                    <p>{education.content}</p>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
 
         <div className="skills">
           <div className="sectionTitle">{strings.skills}</div>
-          <div className='skillsCard'>
+          <div className="skillsCard">
             <ul>
-              {skillsData.map(i => {
+              {skillsData.map((i) => {
                 return (
                   <li key={i.id}>
                     <div>
                       <Image src={i.url} alt={i.name} width={20} height={20} />
-                      {
-                        <p>{i.name}</p>
-                      }
+                      {<p>{i.name}</p>}
                     </div>
                   </li>
                 );
@@ -171,7 +228,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
-
