e0ef1798a4a313bd30e25dd719f1d08cc114b90c
marcosTulli <marcostulli@hotmail.com>
2023-08-23 16:32:20 -0300

    Remove unused lines

--- app/components/home/Home.tsx
+++ app/components/home/Home.tsx
@@ -1,10 +1,9 @@
-// TODO: Use eslint vscode extension
+import styles from './Home.module.scss';
 import React from 'react';
 import Header from '@/app/components/header/Header';
 import WorkExperience from '@/app/components/work-experience/WorkExperience';
 import Qualifications from '@/app/components/qualifications/Qualifications';
 import Info from '@/app/components/info/Info';
-import styles from './Home.module.scss';
 import { useMediaQuery } from '@mui/material';
 
 export default function Home() {
