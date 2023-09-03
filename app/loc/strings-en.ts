096df0ebc6bb021d0b189ec49584280a442e8f0d
marcosTulli <marcostulli@hotmail.com>
2023-09-03 13:24:09 -0300

    Change strings

--- app/loc/strings-en.ts
+++ app/loc/strings-en.ts
@@ -2,7 +2,7 @@ import { Translation } from '@/app/types';
 
 const En: Translation = {
     candidateName: "Marcos Tulli",
-    candidateTitle: "Software engineer / React",
+    candidateTitle: "Software engineer ",
     about: "Experienced and dedicated Front-End Developer with a keen knack for crafting immersive and user-centric web applications using the finesse of ReactJS. Demonstrating a proven history of harmonious cross-functional teamwork, I skillfully transform high-level design concepts into polished digital experiences that resonate with end-users. Fueled by a Bachelor's degree in computer programming from Teclab instituto tecnico, and complemented by PluralSight and Azure certifications, my toolkit boasts proficiency in TypeScript, JavaScript, Next.js, NodeJs, HTML, CSS, and Agile methodologies. Enthusiastic about leveraging my expertise to enhance digital solutions and drive enduring success.",
     workExperience: "Work Experience",
     education: "Education & Certifications",
