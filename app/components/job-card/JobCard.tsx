bd9ec8732a6e84ea3a683730bc072c5fbc60b449
Marcos <marcostulli@hotmail.com>
2024-07-25 11:57:38 -0300

    Remove jobcard icons

--- app/components/job-card/JobCard.tsx
+++ app/components/job-card/JobCard.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
 import { IExperience } from '@/app/models';
 import styles from "./JobCard.module.scss";
-import { useIconKey, useIcons } from '@/app/hooks/queries';
 
 type JobCardProps = {
     experience: IExperience;
@@ -10,25 +8,10 @@ type JobCardProps = {
 };
 
 const JobCard: React.FC<JobCardProps> = ({ experience }) => {
-    const [fileKey, setFileKey] = React.useState("");
-    const formattedname = experience.companyLogo.split('.')[0].split('/')[1];
-    const { data: key } = useIconKey({ name: formattedname });
-    const { data: icon } = useIcons({ fileKey: fileKey });
-    React.useEffect(() => {
-        if (key) {
-            setFileKey(key);
-        }
-    }, [key]);
-
-
     return (
         <div key={experience._id} className={styles.job}>
             <h3 className={styles.jobTitle}>{experience.info.position}</h3>
             <p className={styles.jobDetails}>{experience.activePeriod}</p>
-            {/* <Image src={icon ? icon : ''} alt='phone-icon' width={15} height={15} />
-            <a className={styles.companyName} href={experience.comapnyUrl} target="_blank">
-                <div >{experience.companyName}</div>
-            </a> */}
             <ul className={styles.jobDescription}>
                 {
                     experience.info.tasks.map(task => {
