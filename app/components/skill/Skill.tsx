328ffb31103e4fe185734861fdad68d6518f7e81
Marcos <marcostulli@hotmail.com>
2024-07-26 16:22:17 -0300

    Delete commented code

--- app/components/skill/Skill.tsx
+++ app/components/skill/Skill.tsx
@@ -1,29 +1,15 @@
 'use client';
 import React from 'react';
-// import Image from 'next/image';
 import styles from './Skill.module.scss';
 import { ISkills } from '@/app/models';
-// import { useIcons, useIconKey } from '@/app/hooks/queries';
 import Icon from '../icon/Icon';
-
 
 interface ISkillProps {
     skill: ISkills;
 }
+
 const Skill: React.FC<ISkillProps> = ({ skill }) => {
-    // const [fileKey, setFileKey] = React.useState("");
-    // const { data: key } = useIconKey({ name: skill.name });
-    // const { data: icon } = useIcons({ fileKey: fileKey });
 
-    // React.useEffect(() => {
-    //     if (key) {
-    //         setFileKey(key);
-    //     }
-    // }, [key]);
-
-
-
-    console.log(skill.name);
     return (
         <div className={styles.skill}>
             <Icon name={skill.name} />
