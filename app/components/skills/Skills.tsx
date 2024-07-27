be458dabd181542525ef0879bd65f8ac8b0e32df
Marcos <marcostulli@hotmail.com>
2024-07-27 11:52:58 -0300

    Refacto Skills

--- app/components/skills/Skills.tsx
+++ app/components/skills/Skills.tsx
@@ -1,37 +1,14 @@
 import React from 'react';
 import styles from './Skills.module.scss';
-import Skill from './skill/Skill';
-import { useLanguage } from '@/app/hooks';
-import { userStore } from '@/app/store';
-import { useSkills } from '@/app/hooks/queries';
-import { ISkills } from '@/app/models';
+import SkillsHeader from './SkillsHeader';
+import SkillsBody from './SkillsBody';
 
 
 const Skills = () => {
-    const { strings } = useLanguage();
-    const { user } = userStore();
-    const { data: skillsData } = useSkills({ id: user._id });
-
     return (
         <section className={styles.container}>
-            <div className={styles.skillsHeader}>
-                <h2 className={styles.sectionTitle}>{strings.skills}</h2>
-                <p className={styles.sectionDescription}>
-                    I possess a diverse set of skills that enable me to deliver high-quality work.
-                </p>
-            </div>
-            <div className={styles.skillsGridContainer}>
-                <div className={styles.skillGrid}>
-                    {skillsData?.skills?.map((skill: ISkills) => {
-                        return (
-                            <div key={skill._id} className={styles.skill}>
-                                <Skill skill={skill} />
-                            </div>
-
-                        );
-                    })}
-                </div>
-            </div>
+            <SkillsHeader />
+            <SkillsBody />
         </section>
     );
 };
---
+++ app/components/skills/SkillsBody.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Skills.module.scss';
+import Skill from './skill/Skill';
+import { userStore } from '@/app/store';
+import { useSkills } from '@/app/hooks/queries';
+import { ISkills } from '@/app/models';
+
+const SkillsBody = () => {
+    const { user } = userStore();
+    const { data: skillsData } = useSkills({ id: user._id });
+    return (
+
+        <div className={styles.skillsGridContainer}>
+            <div className={styles.skillGrid}>
+                {skillsData?.skills?.map((skill: ISkills) => {
+                    return (
+                        <div key={skill._id} className={styles.skill}>
+                            <Skill skill={skill} />
+                        </div>
+
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default SkillsBody;
