6c8725e304eedd27f7eef4d3d757f17ab918fc29
Marcos <marcostulli@hotmail.com>
2024-07-27 02:43:41 -0300

    Decomposing components

--- app/components/work-experience/WorkExperience.tsx
+++ app/components/work-experience/WorkExperience.tsx
@@ -1,37 +1,13 @@
 import React from 'react';
-import JobCard from "@/app/components/job-card/JobCard";
-import { useLanguage } from '@/app/hooks';
 import styles from "./WorkExperience.module.scss";
-import { useWorkExperience } from '@/app/hooks/queries';
-import { userStore } from '@/app/store';
-import { IExperience } from '@/app/models';
+import WorkExperienceHeader from './WorkExperienceHeader';
+import WorkExperienceBody from './WorkExperienceBody';
 
-const WorkExperience = () => {
-    const { currentLanguage, strings } = useLanguage();
-    const { user } = userStore();
-    const { data } = useWorkExperience({ id: user._id, lang: currentLanguage });
-    const experiences: IExperience[] = data ? data.experiences : [{ _id: '', activePeriod: '', comapnyUrl: '', companyLogo: '', companyName: '', info: { position: '', tasks: [{ _id: '', task: '' }] } }];
-
+const WorkExperience: React.FC = () => {
     return (
-
         <section className={styles.section}>
-            <div className={styles.sectionHeader}>
-                <h2 className={styles.sectionTitle}>{strings.professionalHistory}</h2>
-                <p className={styles.sectionDescription}>
-                    Explore my professional journey and the diverse roles I have held.
-                </p>
-            </div>
-            <div className={`${styles.flexGrid} py-12 ${styles.gridTwoCols}`}>
-                <div className={styles.flexColumn}>
-                    {experiences.map((experience) => {
-                        return (
-                            <div key={experience._id} className={styles.jobCard}  >
-                                <JobCard experience={experience} language={currentLanguage} />
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
+            <WorkExperienceHeader />
+            <WorkExperienceBody />
         </section>
 
     );
---
+++ app/components/work-experience/WorkExperienceBody.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import JobCard from '../job-card/JobCard';
+import styles from './WorkExperience.module.scss';
+import { useWorkExperience } from '@/app/hooks/queries';
+import { userStore } from '@/app/store';
+import { IExperience } from '@/app/models';
+import { useLanguage } from '@/app/hooks';
+
+
+const WorkExperienceBody = () => {
+    const { currentLanguage } = useLanguage();
+    const { user } = userStore();
+    const { data } = useWorkExperience({ id: user._id, lang: currentLanguage });
+    const experiences: IExperience[] = data ? data.experiences : [{ _id: '', activePeriod: '', comapnyUrl: '', companyLogo: '', companyName: '', info: { position: '', tasks: [{ _id: '', task: '' }] } }];
+
+    return (
+        <div className={`${styles.flexGrid} py-12 ${styles.gridTwoCols}`}>
+            <div className={styles.flexColumn}>
+                {experiences.map((experience) => {
+                    return (
+                        <div key={experience._id} className={styles.jobCard}  >
+                            <JobCard experience={experience} language={currentLanguage} />
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default WorkExperienceBody;
