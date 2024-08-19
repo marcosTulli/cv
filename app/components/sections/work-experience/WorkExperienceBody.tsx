a29569cccc4c66428ae4f6621ad9a58d14a098e8
Marcos <marcostulli@hotmail.com>
2024-08-18 23:05:01 -0300

    Add Grid display on Work Experience

--- app/components/sections/work-experience/WorkExperienceBody.tsx
+++ app/components/sections/work-experience/WorkExperienceBody.tsx
@@ -3,25 +3,25 @@ import JobCard from './job-card/JobCard';
 import styles from './WorkExperience.module.scss';
 import { languageStore } from '@/app/store';
 import { IExperience } from '@/app/models/interfaces';
+import { Container, Grid } from '@mui/material';
 
 interface IWorkExperienceBody {
     data?: IExperience[];
-
 };
 
 const WorkExperienceBody: React.FC<IWorkExperienceBody> = ({ data }) => {
     const { currentLanguage } = languageStore();
 
     return (
-        <div className={`${styles.flexGrid} py-12 ${styles.gridTwoCols}`}>
-            <div className={styles.flexColumn}>
-                {data?.map((experience) => {
-                    return (
-                        <JobCard key={experience._id} experience={experience} language={currentLanguage} />
-                    );
-                })}
-            </div>
-        </div>
+        <Container className={styles.section}>
+            <Grid container maxWidth='lg' spacing={1.5}>
+                {data?.map((experience) => (
+                    <Grid item xs={12} sm={12} md={6} key={experience._id}>
+                        <JobCard experience={experience} language={currentLanguage} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
     );
 };
 
