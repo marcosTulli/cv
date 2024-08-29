7af3138d01753636bf3d77fbc8d2bde57f07c8de
Marcos <marcostulli@hotmail.com>
2024-08-28 22:35:24 -0300

    WIP: Adding theme

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,6 +2,15 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SkeletonTheme } from 'react-loading-skeleton';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#fefefe'
+        }
+    }
+});
 
 const queryClient = new QueryClient();
 
