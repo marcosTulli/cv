4a8a80f2dedb080c0ffff638dc8838f1d49a249d
Marcos <marcostulli@hotmail.com>
2024-09-02 14:12:37 -0300

    WIP Navbar

--- app/components/nav-bar/NavBar.tsx
+++ app/components/nav-bar/NavBar.tsx
@@ -1,4 +1,18 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import styles from './NavBar.module.scss';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import useScroll from '@/app/components/nav-bar/hooks/useScroll';
@@ -7,46 +21,109 @@ import { useIsLoadingSections } from '@/app/hooks';
 import useDownload from './hooks/useDownload';
 import { languageStore } from '@/app/store';
 import ThemePicker from '../theme-picker/ThemePicker';
-import { Box, Button } from '@mui/material';
 
-const NavBar: React.FC = () => {
+interface Props {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window?: () => Window;
+}
+
+const drawerWidth = 240;
+
+export default function DrawerAppBar(props: Props) {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
     const { scroll } = useScroll();
     const { strings } = languageStore();
+
     const sections = Object.keys(Sections).filter((i) =>
         i !== Sections.Header && i !== Sections.PrintableTemplate
     );
     const { handleDownload } = useDownload();
     const { isLoadingSections } = useIsLoadingSections();
 
-    return (
-        <Box className={styles.navContainer}>
-            <HomeOutlinedIcon
-                className={styles.navSection}
-                onClick={() => scroll(Sections.Header)}
-            />
-            {sections.map((section, index) => {
-                return (
-                    <Box
-                        sx={{ color: 'secondary.main' }}
-                        key={index}
-                        className={styles.navSection}
-                        onClick={() => scroll(section)}
-                    >
-                        {section}
-                    </Box>
-                );
-            })}
-            <Button
-                sx={{ color: 'secondary.main' }}
-                disabled={isLoadingSections}
-                className={styles.downloadButton}
-                onClick={handleDownload}
-            >
-                {strings.dropdownOptionsDownload}
-            </Button>
-            <ThemePicker />
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                MUI
+            </Typography>
+            <Divider />
+            <List>
+                {sections.map((item) => (
+                    <ListItem key={item} disablePadding>
+                        <ListItemButton sx={{ textAlign: 'center' }}>
+                            <ListItemText primary={item} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
         </Box>
     );
-};
 
-export default NavBar;
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar component="nav">
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                        {sections.map((section) => (
+                            <Button
+                                sx={{ color: 'secondary.main' }}
+                                key={section}
+                                className={styles.navSection}
+                                onClick={() => scroll(section)}
+
+                            >
+                                {section}
+                            </Button>
+                        ))}
+                    </Box>
+                    <Button
+                        sx={{ color: 'secondary.main' }}
+                        disabled={isLoadingSections}
+                        className={styles.downloadButton}
+                        onClick={handleDownload}
+                    >
+                        {strings.dropdownOptionsDownload}
+                    </Button>
+                    <ThemePicker />
+                </Toolbar>
+            </AppBar>
+            <nav>
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
+        </Box>
+    );
+}
