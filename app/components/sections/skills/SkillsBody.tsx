b82b1b65ad2502cec6101f729bd05217bda8fae3
Marcos <marcostulli@hotmail.com>
2024-09-04 13:17:19 -0300

    Add skills skeleton

--- app/components/sections/skills/SkillsBody.tsx
+++ app/components/sections/skills/SkillsBody.tsx
@@ -19,8 +19,8 @@ const SkillsBody: React.FC<ISkillsBody> = ({ skillsData, isLoading }) => {
                     ? (
                         Array.from({ length: 30 }).map((_, index) => (
                             <div key={index} className={styles.skill}>
-                                <Skeleton circle height={30} width={30} />
-                                <Skeleton height={18} width={76} />
+                                <Skeleton height={30} width={30} />
+                                <Skeleton height={18} width={100} />
                             </div>
                         ))
                     )
