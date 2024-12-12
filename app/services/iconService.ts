59964eecfd2adb584ffe8ac601bfe0b0fb2c4ade
Marcos <marcostulli@hotmail.com>
2024-12-11 21:42:49 -0300

    Fix slash bug

--- app/services/iconService.ts
+++ app/services/iconService.ts
@@ -5,12 +5,11 @@ const location = '/icons';
 const iconService = {
 
     getIconKey: async ({ name }: IGetIconKeyParams): Promise<string> => {
-        return DataProviderInstance.get(`/${location}/${name}`);
+        return DataProviderInstance.get(`${location}/${name}`);
     },
     getIcon: async ({ fileKey }: IGetIconParams): Promise<Blob> => {
         return DataProviderInstance.getCdn(`/${fileKey}`);
     }
-
 };
 
 export default iconService;
