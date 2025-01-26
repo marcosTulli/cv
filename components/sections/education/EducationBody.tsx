443c7fc8c68819aa0cabfa20c9dd51164acd6bd7
Marcos <marcostulli@hotmail.com>
2025-01-26 19:54:45 -0300

    Fix lint issue

--- components/sections/education/EducationBody.tsx
+++ components/sections/education/EducationBody.tsx
@@ -1,32 +1,34 @@
-import * as React from 'react';
-import styles from './Education.module.scss';
-import { IEducation } from '@/app/models/interfaces';
-import EducationCard from './EducationCard';
-import EducationSkeleton from './EducationSkeleton';
-import { Grid } from '@mui/material';
+import * as React from "react";
+import styles from "./Education.module.scss";
+import { IEducation } from "@/models/interfaces";
+import EducationCard from "./EducationCard";
+import EducationSkeleton from "./EducationSkeleton";
+import { Grid } from "@mui/material";
 
 interface IEdcuationBody {
-    data?: IEducation[],
-    isLoading: boolean;
+  data?: IEducation[];
+  isLoading: boolean;
 }
 const EducationBody: React.FC<IEdcuationBody> = ({ data, isLoading }) => {
-
-    return (
-        <Grid
-            container maxWidth='lg' spacing={1.5}
-            sx={{ color: 'secondary.main' }}
-            className={styles.gridContainer}>
-            <div className={styles.educationGrid}>
-                {
-                    isLoading
-                        ? Array.from({ length: 4 })?.map((_, i) => (< EducationSkeleton key={i} />))
-                        : data?.map((school) => (
-                            <EducationCard key={school.id} school={school} />
-                        ))
-                }
-            </div>
-        </Grid>
-    );
+  return (
+    <Grid
+      container
+      maxWidth="lg"
+      spacing={1.5}
+      sx={{ color: "secondary.main" }}
+      className={styles.gridContainer}
+    >
+      <div className={styles.educationGrid}>
+        {isLoading
+          ? Array.from({ length: 4 })?.map((_, i) => (
+              <EducationSkeleton key={i} />
+            ))
+          : data?.map((school) => (
+              <EducationCard key={school.id} school={school} />
+            ))}
+      </div>
+    </Grid>
+  );
 };
 
 export default EducationBody;
