95f3cc8a6e92c7e94964906024fcc9f716b36d3c
Marcos <marcostulli@hotmail.com>
2025-01-26 19:56:27 -0300

    Fix import issue

--- components/sections/education/EducationCard.tsx
+++ components/sections/education/EducationCard.tsx
@@ -1,21 +1,20 @@
-import * as React from 'react';
-import styles from './Education.module.scss';
-import { IEducation } from '@/app/models/interfaces';
+import * as React from "react";
+import styles from "./Education.module.scss";
+import { IEducation } from "@/models/interfaces";
 
 interface IEducationCard {
-    school: IEducation;
+  school: IEducation;
 }
 
 const EducationCard: React.FC<IEducationCard> = ({ school }) => {
-    return (
-        <div className={styles.education}
-        >
-            <div className={styles.educationContent}>
-                <h3 className={styles.educationTitle}>{school.title}</h3>
-                <p className={styles.educationDetails}>{school.content}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.education}>
+      <div className={styles.educationContent}>
+        <h3 className={styles.educationTitle}>{school.title}</h3>
+        <p className={styles.educationDetails}>{school.content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default EducationCard;
