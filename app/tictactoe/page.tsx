778264fd1aa0d05d76d3a9181813a992708df7ff
Marcos <marcostulli@hotmail.com>
2025-03-17 23:14:29 -0300

    Fix bug: Tie message is display when a player wins

--- app/tictactoe/page.tsx
+++ app/tictactoe/page.tsx
@@ -2,107 +2,10 @@
 import React from "react";
 import { Box, Button, Typography } from "@mui/material";
 import styles from "./page.module.scss";
-
-enum Players {
-  X = "X",
-  O = "O",
-}
-
-type Slot = Players.X | Players.O | null;
-
-type Line = {
-  first: Slot;
-  second: Slot;
-  third: Slot;
-};
-
-type Row = Slot[];
-
-const winerLines: [number, number, number][] = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-const grid: Row[] = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
+import useTicTacToe from "./hooks/useTicTacToe";
 
 const TicTac: React.FC = () => {
-  const rows = grid.flatMap((row) => row);
-  const [lockBoard, setLockBoard] = React.useState(false);
-  const [winner, setWinner] = React.useState<Slot>(null);
-  const [turn, setTurn] = React.useState(Players.X);
-  const [slots, setSlots] = React.useState(rows);
-  const [isTie, setIsTie] = React.useState(false);
-
-  const changeTurn = () => {
-    setTurn((p) => (p === Players.X ? Players.O : Players.X));
-  };
-
-  const handleReset = () => {
-    setSlots(rows);
-    setWinner(null);
-    setLockBoard(false);
-    setIsTie(false);
-    if (turn === Players.O) {
-      changeTurn();
-    }
-  };
-
-  const checkSlot = ({ slot, player }: { slot: number; player: Slot }) => {
-    setSlots((prev) => prev.map((num, i) => (i === slot ? player : num)));
-  };
-
-  const validateLine = (line: Line) => {
-    const { first, second, third } = line;
-    return first === second && second === third && third !== null;
-  };
-
-  const won = (player: Slot) => {
-    setLockBoard(true);
-    setWinner(player);
-  };
-
-  const checkWin = (slots: Slot[]) => {
-    for (const [a, b, c] of winerLines) {
-      const line: Line = { first: slots[a], second: slots[b], third: slots[c] };
-      if (validateLine(line)) {
-        won(slots[c]);
-        return;
-      }
-    }
-  };
-
-  const play = (num: number) => {
-    if (lockBoard) {
-      return 0;
-    }
-
-    if (!slots[num]) {
-      if (turn === Players.X) {
-        checkSlot({ slot: num, player: Players.X });
-      } else if (turn === Players.O) {
-        checkSlot({ slot: num, player: Players.O });
-      }
-      changeTurn();
-    }
-  };
-
-  React.useEffect(() => {
-    checkWin(slots);
-    if (!winner && !slots.includes(null)) {
-      setIsTie(true);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [slots, winner]);
+  const { winner, isTie, grid, slots, handleReset, play } = useTicTacToe();
 
   return (
     <Box
