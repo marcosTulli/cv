aadf85180c8e6afd4e1f8112f06dc1e784578765
Marcos <marcostulli@hotmail.com>
2025-07-08 13:53:19 +0200

    Remove admin login

--- components/menu/components/items/Actions.tsx
+++ components/menu/components/items/Actions.tsx
@@ -1,21 +1,14 @@
-import * as React from 'react';
-import { Download, ThemePicker, LanguageSelector } from '../items';
-import OpenAdminDialogButton from '../admin-dialog/button';
-import { Box } from '@mui/material';
-
+import * as React from "react";
+import { Download, ThemePicker, LanguageSelector } from "../items";
 
 const Actions: React.FC = () => {
-    return (
-        <>
-            <LanguageSelector />
-            <ThemePicker />
-            <Download />
-            <Box
-                sx={{ padding: '0px', margin: '0px', display: { lg: 'none', md:'none' } }}>
-                <OpenAdminDialogButton />
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <LanguageSelector />
+      <ThemePicker />
+      <Download />
+    </>
+  );
 };
 
 export default Actions;
