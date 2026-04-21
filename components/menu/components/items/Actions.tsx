de9f9ca6f33abcd542f924053c48590d107a0bf1
Marcos <marcostulli@hotmail.com>
2026-04-21 11:58:19 -0300

    Refactor edit cta placement

--- components/menu/components/items/Actions.tsx
+++ components/menu/components/items/Actions.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
+import { Divider } from '@mui/material';
 import { Download, ThemePicker, LanguageSelector, EditModeSwitch } from '../items';
+import { useAuth } from '@/hooks';
 
 const Actions: React.FC = () => {
+  const { isAdmin } = useAuth();
+
   return (
     <>
       <LanguageSelector />
       <ThemePicker />
       <Download />
+      {isAdmin && (
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{ mx: 1, borderColor: 'rgba(255,255,255,0.2)' }}
+        />
+      )}
       <EditModeSwitch />
     </>
   );
